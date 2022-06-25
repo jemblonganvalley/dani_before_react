d7f0241c28ff3b8555a9e03311e0b1663811057d
jemblonganvalley <jemblonganvalley@gmail.com>
2022-06-25 21:03:19 +0700

    template literal

---
+++ templateLiteral.js
@@ -0,0 +1,8 @@
+let username = "fadliselaz"
+let address = "depok"
+let phone = "081213655573"
+
+let text = `nama saya ${username} alamat saya di ${address} telpon saya ${phone}`
+
+
+console.info(text)
