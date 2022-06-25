bc037e6afec2bc2225c9f1d99c2f516de1d7869c
jemblonganvalley <jemblonganvalley@gmail.com>
2022-06-25 20:53:44 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,23 @@
+// variable dengan var
+// bisa di redeclare / dibuat lagi dengan nama variable yang sama
+// bisa di reassign / diganti isi dari variablenya
+// scope global
+var username = "fadliselaz"
+var username = "evalia" // tidak error
+
+// variable dengan let
+// tidak bisa di redeclare / dibuat lagi dengan nama variable yang sama
+// bisa di reassign / diganti isi dari variablenya
+let address = "depok"
+let address = "jakarta" // error 
+address = "bogor" // tidak error
+
+// variable dengan const
+// tidak bisa di redeclare / dibuat lagi dengan nama variable yang sama
+// tidak bisa di reassign / diganti isi dari variablenya
+const hoby = "ngoding"
+const hoby = "game" // error
+hoby = "makan" // error
+
+
+
